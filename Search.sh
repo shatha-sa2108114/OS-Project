643934d85ea475b04f2f49b873ff47d7e726db28
reema-ra2103056 <ra2103056@qu.edu.qa>
2024-11-02 20:10:02 +0300

    Update Search.sh 2

--- Search.sh
+++ Search.sh
@@ -4,7 +4,7 @@ bigfile="bigfile"
 system_admin_email="reemakib0@gmail.com"  
 sender_email="operatingsys2024@gmail.com"  
 
-find . -type f -size +1M > "$bigfile"
+find "$HOME" -type f -size +1M > "$bigfile"
 echo "current date: $(date '+%Y-%m-%d %H:%M:%S')" >> "$bigfile"
 echo "number of files fo: $(wc -l < "$bigfile")" >> "$bigfile"
 
